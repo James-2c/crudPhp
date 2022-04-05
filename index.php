e85c9ece5a29b0550500bde1ae796bda564ea2d9
James-2c <lanricjamesdeguzman.basc@gmail.com>
2022-04-05 15:07:42 +0000

    back ver

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
+<?php
 
+include_once('connection/connection.php');
+
+$con = connection();
+
+if(!isset($_SESSION)){
+    session_start();
+}
+
+if(!isset($_SESSION['Username'])){
+    echo header("Location: login.php");
+}
+
+$sql = "SELECT * FROM STUDENT";
+$result = $con->query($sql) or die ($con->error);
+$row = $result->fetch_assoc();
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +51,32 @@
     <br>
     <a href="add.php" class="btn">Add</a>   
 
-    
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Firstname</th>
+                <th>Initial</th>
+                <th>Lastname</th>
+                <th>Sex</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php do{ ?>
+            <tr>        
+                <td><?php echo $row["Student_id"] ?></td>
+                <td><?php echo $row["Student_fname"] ?></td>
+                <td><?php echo $row["Student_init"] ?></td>
+                <td><?php echo $row["Student_lname"] ?></td>
+                <td><?php echo $row["Student_sex"] ?></td>
+                <td ><a class="view" href="profile.php?id=<?php echo $row["Student_id"]; ?>">View</a></td>
+                <td ><a class="delete" href="delete.php?id=<?php echo $row["Student_id"]; ?>">Delete</a></td>        
+            </tr>
+            <?php }while($row = $result->fetch_assoc()); ?>
+        </tbody>
+    </table>
    
 </body>
 </html>
